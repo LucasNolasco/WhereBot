31dd799fd379f2a06079144fc7489dd2c9c4f0cf
WagnerRua <wagner.rua@hotmail.com>
2020-10-12 00:12:25 +0200

    Melhorando simulacao do wifi no pacote Survey

--- survey/src/simulation.py
+++ survey/src/simulation.py
@@ -138,6 +138,26 @@ class WirelessSimulation:
         rospy.loginfo("Movimentação finalizada. Salvando mapa e CSV.")
 
     # ----------------------------------------------------------
+    #   isFree
+    #       Verifica se uma determinada posição possui obstáculo
+    #
+    #   Parâmetros:
+    #       x: Coordenada do robô no eixo x
+    #       y: Coordenada do robô no eixo y
+    #
+    #   Retorno: True, caso não haja obstáculo naquela posição
+    #            False, caso haja obstáculo naquela posição
+    #
+    # ----------------------------------------------------------
+    def isFree(self, x, y):
+        x = int(x)
+        y = int(y)
+        if self.costmap[y][x] >= 0 and self.costmap[y][x] < 100:
+            return True
+        else:
+            return False
+
+    # ----------------------------------------------------------
     #   distance
     #       Calcula a distância euclidiana entre dois pontos
     #
@@ -181,9 +201,47 @@ class WirelessSimulation:
         return ( ((positionTuple[0] + 0.5 )*self.scale + self.origin_x), 
                  ((positionTuple[1] + 0.5 )*self.scale + self.origin_y))
 
+    def countBlackPixels(self, robotPosition, apPosition):
+        x0 = robotPosition[0]
+        y0 = robotPosition[1]
+        x1 = apPosition[0]
+        y1 = apPosition[1]
+        
+        mat = self.costmap
+
+        # Swap axes if Y slope is smaller than X slope
+        transpose = abs(x1 - x0) < abs(y1 - y0)
+        if transpose:
+            mat = mat.T
+            x0, y0, x1, y1 = y0, x0, y1, x1
+        # Swap line direction to go left-to-right if necessary
+        if x0 > x1:
+            x0, y0, x1, y1 = x1, y1, x0, y0
+        
+
+        # Compute intermediate coordinates using line equation
+        x = np.arange(x0 + 1, x1)
+        y = np.round(((y1 - y0) / (x1 - x0)) * (x - x0) + y0).astype(x.dtype)
+        
+        countPixels = 0
+        for i in range(len(x)):
+            if mat[y[i]][x[i]] > 250:
+                countPixels += 1
+
+        return countPixels
+        
     def dbmCalculator(self, robotPosition, apPosition):
+        blackPixels = self.countBlackPixels(robotPosition, apPosition)
         distance = self.distance(self.pixelToMeters(robotPosition), self.pixelToMeters(apPosition))
-        return (-int(20*log10(distance) - FSPL + 20*log10(MHz)))
+
+        dbm = (-int(20*log10(distance) - FSPL + 20*log10(MHz)))
+        
+        if blackPixels < 18:
+            dbm += (blackPixels/3)*(-2)
+        else:
+            dbm += -15
+
+        return int(dbm)
 
     def setVariablesCSV(self):
         self.dbm_csv_path = '/home/wagner/catkin_ws/src/survey/simulation_files/' + str(random.randint(1,100000)) + '.csv'
@@ -209,6 +267,8 @@ class WirelessSimulation:
         self.pub_output = rospy.Publisher("/survey/heatmap/input", String, queue_size=10)
 
         self.tf_listener = tf.TransformListener() # Instancia o objeto para lidar com as transformações de posição no mapa
+        
+        self.setVariablesCSV()
 
         rate = rospy.Rate(0.5)
         counter = 1
